b29435017bc7eba040be1508b5c076472181b494
Vasyl Kliushta <castelkwm@gmail.com>
2022-08-20 21:19:16 +0300

    met with buttons

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 
-
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import {Typography} from "@mui/material";
+import Button from '@mui/material/Button';
+import {Container} from "@mui/material";
 
 export default function Create() {
     return (
-        <div>
-            Create page
-        </div>
+        <Container>
+
+            <Typography
+                variant="h6"
+                color="textSecondary"
+                component="h2"
+            >
+                create new
+            </Typography>
+            <Button
+                onClick={()=>
+            console.log('works')}
+                type="submit"
+                color="secondary"
+                variant="contained"
+            >submit</Button>
+            {/*<Button type="submit" color="primary">submit</Button>
+            <Button type="submit" color="secondary" variant="outlined">submit</Button>
+
+            */}
+
+        </Container>
     )
 }
